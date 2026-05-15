a201756df269a8768367eda7b18cf36f00015695
Brun SOMASSE <brunsomasse@gmail.com>
2026-05-15 18:59:37 +0000

    Update UAT and PROD to use new networking module with Internal ALB

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,23 +1,33 @@
 terraform {
   required_version = ">= 1.5"
+  backend "s3" {
+    bucket         = "hiive-terraform-state-bucket"
+    key            = "prod/eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "hiive-terraform-state-lock"
+  }
 }
 
-module "vpc" {
-  source = "../../modules/vpc"
+module "networking" {
+  source = "../../modules/networking"
 
   environment     = "prod"
   region          = var.region
+  project         = var.project
   vpc_cidr        = "10.30.0.0/16"
   private_subnets = ["10.30.1.0/24", "10.30.2.0/24"]
   public_subnets  = ["10.30.101.0/24", "10.30.102.0/24"]
+  cluster_name    = "${var.project}-prod-cluster"
+  eks_dependency  = module.eks
 }
 
 module "eks" {
   source = "../../modules/eks"
 
   environment     = "prod"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
+  vpc_id          = module.networking.vpc_id
+  private_subnets = module.networking.private_subnets
   node_min        = 3
   node_max        = 6
   node_desired    = 3
@@ -25,8 +35,4 @@ module "eks" {
 
 module "app" {
   source = "../../modules/app"
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
 }
--- environments/uat/main.tf
+++ environments/uat/main.tf
@@ -1,23 +1,33 @@
 terraform {
   required_version = ">= 1.5"
+  backend "s3" {
+    bucket         = "hiive-terraform-state-bucket"
+    key            = "uat/eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "hiive-terraform-state-lock"
+  }
 }
 
-module "vpc" {
-  source = "../../modules/vpc"
+module "networking" {
+  source = "../../modules/networking"
 
   environment     = "uat"
   region          = var.region
+  project         = var.project
   vpc_cidr        = "10.20.0.0/16"
   private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
   public_subnets  = ["10.20.101.0/24", "10.20.102.0/24"]
+  cluster_name    = "${var.project}-uat-cluster"
+  eks_dependency  = module.eks
 }
 
 module "eks" {
   source = "../../modules/eks"
 
   environment     = "uat"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
+  vpc_id          = module.networking.vpc_id
+  private_subnets = module.networking.private_subnets
   node_min        = 2
   node_max        = 4
   node_desired    = 2
@@ -25,8 +35,4 @@ module "eks" {
 
 module "app" {
   source = "../../modules/app"
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
 }
